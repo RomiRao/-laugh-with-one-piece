ef50387ab1654017b57d6dc6bda34058b32cdf11
RomiRao <raoromina96@gmail.com>
2023-06-29 22:24:39 -0300

    adding sounds :D

---
+++ script/script.js
@@ -0,0 +1,91 @@
+const audioEtiqueta = document.querySelector("audio");
+
+//Buttons declared (in order)
+const buttonJaguar = document.querySelector("#jaguar-laugh");
+const buttonCesar = document.querySelector("#cesar-laugh");
+const buttonKaido = document.querySelector("#kaido-laugh");
+const buttonMama = document.querySelector("#mama-laugh");
+const buttonLuffy = document.querySelector("#luffy-laugh");
+const buttonBrook = document.querySelector("#brook-laugh");
+const buttonDoffy = document.querySelector("#doffy-laugh");
+const buttonPerona = document.querySelector("#perona-laugh");
+const buttonShirohige = document.querySelector("#shirohige-laugh");
+const buttonCroco = document.querySelector("#croco-laugh");
+const buttonTrebol = document.querySelector("#trebol-laugh");
+const buttonMoria = document.querySelector("#moria-laugh");
+const buttonNeko = document.querySelector("#neko-laugh");
+const buttonRoger = document.querySelector("#roger-laugh");
+
+
+//Sound Effects (in order)
+
+buttonJaguar.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Jaguar.mp3")
+    audioEtiqueta.play()
+});
+
+buttonCesar.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Cesar.mp3")
+    audioEtiqueta.play()
+});
+
+buttonKaido.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Kaido.mp3")
+    audioEtiqueta.play()
+});
+
+buttonMama.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Mama.mp3")
+    audioEtiqueta.play()
+});
+
+buttonLuffy.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Luffy.mp3")
+    audioEtiqueta.play()
+});
+
+buttonBrook.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Brook.mp3")
+    audioEtiqueta.play()
+});
+
+buttonDoffy.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Doffy.mp3")
+    audioEtiqueta.play()
+});
+
+buttonPerona.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Perona.mp3")
+    audioEtiqueta.play()
+});
+
+buttonShirohige.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Shirohige.mp3")
+    audioEtiqueta.play()
+});
+
+buttonCroco.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Croco.mp3")
+    audioEtiqueta.play()
+});
+
+buttonTrebol.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Trebol.mp3")
+    audioEtiqueta.play()
+});
+
+buttonMoria.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Moria.mp3")
+    audioEtiqueta.play()
+});
+
+buttonNeko.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Neko.mp3")
+    audioEtiqueta.play()
+});
+
+buttonRoger.addEventListener("click", () => {
+    audioEtiqueta.setAttribute("src", "../resources/audio/Roger.mp3")
+    audioEtiqueta.play()
+});
+
