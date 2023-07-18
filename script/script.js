040db0ffd2cda8ad2b4c7cd48fe8146b1c11c66f
RomiRao <raoromina96@gmail.com>
2023-07-17 23:27:32 -0300

    fixing audio bug

--- script/script.js
+++ script/script.js
@@ -16,76 +16,116 @@ const buttonMoria = document.querySelector("#moria-laugh");
 const buttonNeko = document.querySelector("#neko-laugh");
 const buttonRoger = document.querySelector("#roger-laugh");
 
-
 //Sound Effects (in order)
 
 buttonJaguar.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Jaguar.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Jaguar.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonCesar.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Cesar.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Cesar.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonKaido.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Kaido.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Kaido.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonMama.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Mama.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Mama.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonLuffy.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Luffy.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Luffy.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonBrook.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Brook.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Brook.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonDoffy.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Doffy.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Doffy.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonPerona.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Perona.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Perona.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonShirohige.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Shirohige.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Shirohige.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonCroco.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Croco.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Croco.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonTrebol.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Trebol.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Trebol.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonMoria.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Moria.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Moria.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonNeko.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Neko.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Neko.mp3"
+    );
+    audioEtiqueta.play();
 });
 
 buttonRoger.addEventListener("click", () => {
-    audioEtiqueta.setAttribute("src", "../resources/audio/Roger.mp3")
-    audioEtiqueta.play()
+    audioEtiqueta.setAttribute(
+        "src",
+        "../-laugh-with-one-piece/resources/audio/Roger.mp3"
+    );
+    audioEtiqueta.play();
 });
-
